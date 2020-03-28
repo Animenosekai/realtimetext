b37de66f80cd7a1f618b6c8d2b84bbc418a32384
Animenosekai <maxence.osawa@gmail.com>
2020-03-28 01:37:30 +0100

    Initial Release!

---
+++ main.js
@@ -0,0 +1,58 @@
+function createDoc(){
+    axios({
+        method: 'post',
+        url: 'https://jsonblob.com/api/jsonBlob',
+    })
+    .then(function(response){
+        var document_url = response.headers.location
+        window.localStorage.setItem('document_url', document_url)
+        document.getElementById('document_url').innerHTML = 'The url of this document is: ' + "127.0.0.1:5500/main.html?id=" + document_url.replace('https://jsonblob.com/api/jsonBlob/', '')
+        console.log('Document created!')
+    })
+}
+
+var getTextInterval
+function getText(){
+    getTextInterval = setInterval(function(){
+    axios({
+        method: 'get',
+        url: window.localStorage.getItem('document_url')
+    })
+    .then(function(response){
+        var newText = response.data.text
+        document.getElementById('mainText').value = newText
+    })
+    },1000)
+}
+
+function putText(){
+    var proceed = window.localStorage.getItem('proceed')
+    if(proceed === 'true'){
+    window.localStorage.setItem('proceed', 'false')
+    clearInterval(getTextInterval)
+    var sentText = document.getElementById('mainText').value
+    axios({
+        method: 'put',
+        url: window.localStorage.getItem('document_url'),
+        data: {
+            text: sentText
+        }
+    })
+    .then(function(){
+        window.localStorage.setItem('proceed', 'true')
+        console.log('Data send!')
+        setTimeout(getText(), 100)
+    })
+}else if(proceed === false){
+    setTimeout(putText(),100)
+}
+}
+
+window.onload = function(){
+    const urlParams = new URLSearchParams(window.location.search);
+    const documentID = urlParams.get('id');
+    window.localStorage.setItem('document_url', 'https://jsonblob.com/api/jsonBlob/' + documentID);
+    document.getElementById('document_url').innerHTML = 'The url of this document is: ' + "127.0.0.1:5500/main.html?id=" + documentID
+    window.localStorage.setItem('proceed', 'true');
+    console.log('Initialization...')
+}
